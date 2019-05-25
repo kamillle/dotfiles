79908b8a3522ca72167647ce199aea6b07b3073f
kamille-gz <yuji.kmjm@gmail.com>
2019-05-25 16:35:32 +0900

    brew -v deleted

--- bin/dev_setup.sh
+++ bin/dev_setup.sh
@@ -7,7 +7,6 @@ case "$yn" in [yY]*) ;; *) echo "abort." ; exit ;; esac
 if (type brew > /dev/null 2>&1) ; then
   echo "======= Install Homebrew ======="
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  brew -v
   echo "============= End =============="
 fi
 
