b31c2edb698d418e5fe6638de51c5dd0747f85c8
kamillle <yuji.kmjm@gmail.com>
2019-05-25 23:49:04 +0900

    refactor case statement

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
 read -p "start dev setup? (y/N): " yn
-case "$yn" in [yY]*) ;; *) echo "abort." ; exit ;; esac
+case "$yn" in
+  [yY]*)
+    ;;
+  *)
+    echo "abort." ; exit
+    ;;
+esac
 
 # install homebrew
 if (type brew > /dev/null 2>&1) ; then
