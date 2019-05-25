b6261834427a131f0fdd56cb47db81e4ef784293
kamille-gz <yuji.kmjm@gmail.com>
2019-05-25 20:49:17 +0900

    fix lines

--- bin/dev_setup.sh
+++ bin/dev_setup.sh
@@ -6,16 +6,21 @@ case "$yn" in [yY]*) ;; *) echo "abort." ; exit ;; esac
 # install homebrew
 if (type brew > /dev/null 2>&1) ; then
   echo "======= Install Homebrew ======="
+
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
   echo "============= End =============="
 fi
 
 # install git
 if brew list | grep git > /dev/null; then
   echo "======= Install git ======="
+
   brew install git
+
   echo "============= End =============="
 fi
+
 
 # dotfiles/.bashrc を読み込みbrew cask でのインストール先を /Applications に変更する
 cp .bashrc ~/.bashrc
@@ -61,6 +66,7 @@ brew cask install docker
 
 echo "============= End =============="
 
+
 echo "======= Install & setting zsh ======="
 
 brew install zsh
@@ -86,6 +92,7 @@ brew install tree
 
 echo "========== End =========="
 
+
 echo "======= Setup Neovim ======="
 
 # denite, deopleteが python3を必要とするので先にpythonのセッティングを行う
@@ -110,6 +117,7 @@ brew install ripgrep
 brew cask install font-hack-nerd-font
 
 echo "============ End ============="
+
 
 echo "======= Setup Ruby ======="
 
