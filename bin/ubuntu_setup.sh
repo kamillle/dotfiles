67fc53748c843bf82b381c49da98a6ba45ad84b1
kamillle <32205171+kamillle@users.noreply.github.com>
2019-11-25 21:09:47 +0900

    Start Ubuntu!!

---
+++ bin/ubuntu_setup.sh
@@ -0,0 +1,202 @@
+#!/bin/sh
+
+read -p "start dev setup? (y/N): " yn
+case "$yn" in
+  [yY]*)
+    ;;
+  *)
+    echo "abort." ; exit
+    ;;
+esac
+
+echo "======= Update apt ======="
+
+sudo apt update
+sudp apt upgrade
+
+echo "====== Update apt ======"
+
+# install git
+if sudo apt list --installed git | grep Done > /dev/null; then
+  echo "======= Install git ======="
+
+  sudo apt install git
+
+  echo "========== End ============"
+fi
+
+echo "======= Setup git ======="
+
+ln -snfv ${HOME}/dotfiles/git_setting/gitconfig ${HOME}/.gitconfig
+ln -snfv ${HOME}/dotfiles/git_setting/gitignore_global ${HOME}/.gitignore_global
+
+echo "===== End ====="
+
+
+echo "===== pbcopy behavior ====="
+if sudo apt list --installed git | grep Done > /dev/null; then
+  sudo apt-get install xsel
+fi
+echo "===== End ====="
+
+echo "======= Install Applications ======="
+
+# brew install mas # App StoreのアプリをCLIでダウンロードできるようにする
+# mas install 497799835  # Xcode
+# mas install 405399194  # Kindle
+# mas install 425955336  # Skitch
+# mas install 485812721  # TweetDeck
+# mas install 1295203466 # Microsoft Remote Desktop (10.2.13)
+# mas install 539883307  # LINE
+# mas install 417375580  # BetterSnapTool
+#
+# brew cask install google-chrome-canary
+# brew cask install google-japanese-ime
+# brew cask install firefox
+# brew cask install homebrew/cask-versions/sequel-pro-nightly # mysql GUI for mojave
+# brew cask install slack       # slackたん
+# brew cask install typora      # md editor
+# brew cask install cmd-eikana  # 英かな使えるかわからん
+# brew cask install alfred      # lancher
+# brew cask install hyperswitch # cmd + tab でwindow毎に切り替え
+# brew cask install appcleaner  # remove applications
+# brew cask install dropbox     # share files
+# brew cask install clipy       # extend pdcopy
+# brew cask install dash        # リファレンス見る
+# brew cask install iterm2      # CLI
+# brew cask install karabiner   # customize key mapping
+# brew cask install evernote    # memo
+# brew cask install skitch      # edit images
+# # dockerは brew, brew cask 両方必要
+# brew install docker
+# brew cask install docker
+
+echo "============= End =============="
+
+
+echo "======= Setup zsh ======="
+
+# brew install zsh
+# brew install zsh-completions
+#
+# sudo sh -c "echo /usr/local/bin/zsh >> /etc/shells"
+# # change login shell
+# chsh -s /usr/local/bin/zsh
+#
+ln -snfv ${HOME}/dotfiles/ubuntu/.zshrc ${HOME}/.zshrc
+#
+source ${HOME}/.zshrc
+
+echo "================ End ================"
+
+echo "======= Setup tmux ======="
+
+brew install tmux
+brew install reattach-to-user-namespace
+
+ln -snfv ${HOME}/dotfiles/.tmux.conf ${HOME}/.tmux.conf
+
+tmux source ${HOME}/.tmux.conf
+
+echo "================ End ================"
+
+echo "======= Setup CLI ======="
+
+brew install peco
+brew install htop
+brew install hub
+brew install tig
+brew install trash
+brew install tree
+brew install translate-shell # shellからgoogle翻訳
+brew install gnu-sed
+
+echo "========== End =========="
+
+echo "======= Setup Neovim ======="
+
+# denite, deopleteが python3を必要とするので先にpythonのセッティングを行う
+PYTHON_VERSION=3.7.1
+brew install pyenv
+pyenv install PYTHON_VERSION
+pyenv global PYTHON_VERSION
+
+brew install neovim
+# deniteセットアップのためのシェルスクリプトをダウンロード & 実行
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
+# For example, we just use `~/.cache/dein` as installation directory
+sh ./installer.sh ~/.cache/dein
+
+# set sym link
+ln -snfv ${HOME}/dotfiles/.vimrc ${HOME}/.config/nvim/init.vim
+
+# for incremental search by fzf
+brew install ripgrep
+
+# make rich vim icons
+brew cask install font-hack-nerd-font
+
+# monaco powerline
+wget https://gist.github.com/baopham/1838072/raw/2c0e00770826e651d1e355962e751325edb0f1ee/Monaco%20for%20Powerline.otf
+mv Monaco\ for\ Powerline.otf /Library/Fonts/
+
+echo "============ End ============="
+
+
+echo "======= Setup Ruby ======="
+
+brew install rbenv ruby-build
+
+brew install readline
+brew link readline --force
+RUBY_CONFIGURE_OPTS="--with-readline-dir=$(brew --prefix readline)"
+echo 'export PATH="$HOME/.rbenv/bin/:$PATH" >> ~/.zshrc'
+echo 'eval "$(rbenv init -)" >> ~/.zshrc'
+
+# neovimが ruby2.6.2 と neovim gem を参照するので入れておく
+# 2.6.2 が古くなったらvimrcに書いてあるneovimが参照するバージョンを変更すること
+rbenv install 2.6.2
+rbenv global 2.6.2
+gem install neovim
+
+# 最新版をインストールする
+LATEST_STABLE_RUBY = $(rbenv install -l | grep -v - | tail -1)
+rbenv install LATEST_STABLE_RUBY
+rbenv global LATEST_STABLE_RUBY
+gem install bundler
+rbenv rehash
+
+ln -snfv ${HOME}/dotfiles/.pryrc ${HOME}/.pryrc
+
+echo "========== End ==========="
+
+
+echo "======= Setup golang ======="
+
+# 最新版のgoをインストールする
+LATEST_STABLE_GO = $(goenv install -l | grep -E "([1].[0-9].[0-9])|([1].[0-9][0-9].[0-9])" | tail -1)
+brew install goenv
+goenv install LATEST_STABLE_GO
+goenv global LATEST_STABLE_GO
+
+echo "========== End ==========="
+
+echo "======= Setup golang ======="
+
+brew install nvm
+mkdir ${HOME}/.nvm
+
+nvm install v10.13.0
+node -v
+nvm use 10.13.0
+
+# make available git open command
+npm install --global git-open
+
+echo "========== End ==========="
+
+# echo "======= Setup mac config ======="
+
+# source ./mac_config.sh
+
+# echo "============= End =============="
---
+++ ubuntu/.zshrc
@@ -0,0 +1,170 @@
+export LANG=ja_JP.UTF-8 # 日本語を使用
+setopt no_beep
+# use vi mode
+# bindkey -v
+
+#----------------------------------------------------------
+# completion
+#----------------------------------------------------------
+autoload -U compinit
+compinit -u
+zstyle ':completion:*:default' menu select=1        # 選択肢をハイライト
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}' # 大文字、小文字を区別せず補完する
+zstyle ':completion::complete:*' use-cache true     # キャッシュの利用による補完の高速化
+# 補完候補を色付け
+autoload -U colors
+colors
+
+#----------------------------------------------------------
+# prompt
+#----------------------------------------------------------
+# VCSの情報を取得するzsh関数
+autoload -Uz vcs_info
+
+# PROMPT変数内で変数参照
+setopt prompt_subst
+
+# %b ブランチ情報
+# %a アクション名(mergeなど)
+# %c changes
+# %u uncommit
+
+zstyle ':vcs_info:git:*' check-for-changes true          # formats 設定項目で %c,%u が使用する true にすると重い...
+zstyle ':vcs_info:git:*' unstagedstr "%F{blue}A+"        # add されていないファイルがある
+zstyle ':vcs_info:git:*' stagedstr "%F{red}C!"           # commit されていないファイルがある
+zstyle ':vcs_info:*'     formats "%F{white}-> %c%u%b%f " # 通常
+zstyle ':vcs_info:*'     actionformats '(%b|%a)'         # rebase 途中,merge コンフリクト等 formats 外の表示
+
+# プロンプト表示直前に vcs_info 呼び出し
+precmd () { vcs_info }
+PROMPT='%{$fg_bold[yellow]%} %~% %{$reset_color%} ${vcs_info_msg_0_}%{$reset_color%}
+%{${fg_bold[red]}%}%}  $ % %{${reset_color}%'
+
+#----------------------------------------------------------
+# alias
+#----------------------------------------------------------
+# alias py="python"
+# alias ipy="ipython"
+# alias es="elasticsearch"
+# alias be="bundle exec"
+# alias ber="bundle exec rubocop"
+# alias sr="bundle exec spring rspec"
+# alias rm="trash"
+# alias agg="ag -g"
+# alias ls="ls -G"
+# alias ctags="`brew --prefix`/bin/ctags"
+alias v="nvim"
+alias vim="nvim"
+alias vi="nvim"
+alias ac="git add . && git cm"
+alias c="clear"
+alias pbcopy='xsel --clipboard --input' # @see https://qiita.com/yoshikyoto/items/1676b925580717c0a443
+
+#----------------------------------------------------------
+# command history
+#----------------------------------------------------------
+HISTFILE=~/.zsh_history
+HISTSIZE=20000
+SAVEHIST=20000
+# ignore duplication command history list
+setopt hist_ignore_dups
+# share command history data
+setopt share_history
+
+#----------------------------------------------------------
+# environment variable
+#----------------------------------------------------------
+export ZPLUG_HOME=/usr/local/opt/zplug
+source $ZPLUG_HOME/init.zsh
+export PATH="/usr/local/bin":$PATH
+
+# Ruby
+eval "$(rbenv init -)"
+
+# Python
+export PYENV_ROOT="${HOME}/.pyenv"
+export PATH=${PYENV_ROOT}/bin:$PATH
+eval "$(pyenv init -)"
+# eval "$(pyenv virtualenv-init -)"
+
+# Go
+export GOPATH=$HOME/go
+export GOENV_ROOT=$HOME/.goenv
+export GO_WORKSPACE=$HOME/go/src/github.com/kamillle
+export PATH="$GOENV_ROOT/bin:$PATH"
+eval "$(goenv init -)"
+export PATH="$GOROOT/bin:$PATH"
+export PATH="$GOPATH/bin:$PATH"
+
+# elastic
+#export PATH="/usr/local/opt/elasticsearch@5.6/bin:$PATH"
+
+# psql
+export PGDATA="/usr/local/var/postgresql@9.6"
+export PATH="/usr/local/opt/postgresql@9.6/bin:$PATH"
+
+export PATH="$HOME/.gem/ruby/2.0.0/bin:$PATH"
+export PATH="/usr/local/opt/mysql@5.7/bin:$PATH"
+
+# java
+export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
+export PATH="/usr/local/opt/imagemagick@6/bin:$PATH"
+
+# chromedriver
+export CHROMEDRIVER_PATH='/usr/local/bin/chromedriver'
+
+# neovim
+export XDG_CONFIG_HOME="$HOME/.config"
+
+# direnv
+eval "$(direnv hook zsh)"
+
+export PATH="/usr/local/opt/openssl/bin:$PATH"
+
+# yarn
+export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
+
+# nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/usr/local/opt/nvm/etc/bash_completion" ] && . "/usr/local/opt/nvm/etc/bash_completion"  # This loads nvm bash_completion
+
+# pecoを優先するためコメントアウト
+# [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+#----------------------------------------------------------
+# define functions
+#----------------------------------------------------------
+# ctrl + r でコマンド履歴一覧
+function peco-history-selection() {
+    BUFFER=`history -n 1 | tail -r  | awk '!a[$0]++' | peco`
+    CURSOR=$#BUFFER
+    zle reset-prompt
+}
+zle -N peco-history-selection
+bindkey '^R' peco-history-selection
+
+# grepした上でそのファイルをvimで開く
+function gg () {
+  vim $(ag $@ | peco --query "$LBUFFER" | awk -F : '{print "-c " $2 " " $1}')
+}
+
+# 遅いのでコメントアウトしてる
+# hubコマンドをgitコマンドとして使用
+# see: https://qiita.com/yaotti/items/a4a7f3f9a38d7d3415e3
+# function git(){hub "$@"}
+
+# snakecase への変換
+function snake() {
+  echo $1 | gsed -r -e 's/^([A-Z])/\L\1\E/' -e 's/([A-Z])/_\L\1\E/g' | pbcopy
+}
+
+# upper camel への変換
+function camel() {
+  echo $1 | gsed -r -e 's/(^|_)(.)/\U\2\E/g' | pbcopy
+}
+
+# springで起動しているrails appを全部殺すマン
+function kspring() {
+  pkill spring
+}
