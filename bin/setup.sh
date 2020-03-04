2ddfce2bb55c66b2d983e87a5d35ef0856ca96ad
kamillle <32205171+kamillle@users.noreply.github.com>
2020-03-05 01:24:09 +0900

    Install goenv by git clone

--- bin/setup.sh
+++ bin/setup.sh
@@ -191,9 +191,9 @@ echo "========== End ==========="
 
 echo "======= Setup golang ======="
 
+git clone https://github.com/syndbg/goenv.git ~/.goenv
 # 最新版のgoをインストールする
 LATEST_STABLE_GO = $(goenv install -l | grep -E "([1].[0-9].[0-9])|([1].[0-9][0-9].[0-9])" | tail -1)
-brew install goenv
 goenv install LATEST_STABLE_GO
 goenv global LATEST_STABLE_GO
 
