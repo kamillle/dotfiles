1bf254a5bb35a59e070d607154d1fa20c5e72205
kamillle <32205171+kamillle@users.noreply.github.com>
2021-03-13 16:45:00 +0900

    Separate ruby setup

--- bin/setup.sh
+++ bin/setup.sh
@@ -136,32 +136,6 @@ brew install ghq
 mkdir ~/ghq
 echo "====== End ======"
 
-echo "====== Setup Ruby ======"
-brew install rbenv ruby-build
-
-brew install readline
-brew link readline --force
-RUBY_CONFIGURE_OPTS="--with-readline-dir=$(brew --prefix readline)"
-echo 'export PATH="$HOME/.rbenv/bin/:$PATH" >> ~/.zshrc'
-echo 'eval "$(rbenv init -)" >> ~/.zshrc'
-
-# neovimが ruby2.7.1 と neovim gem を参照するので入れておく
-# 2.7.1が古くなったらvimrcに書いてあるneovimが参照するバージョンを変更すること
-rbenv install 2.7.1
-rbenv global 2.7.1
-gem install neovim
-
-# 最新版をインストールする
-LATEST_STABLE_RUBY = $(rbenv install -l | grep -v - | tail -1)
-rbenv install LATEST_STABLE_RUBY
-rbenv global LATEST_STABLE_RUBY
-gem install bundler
-rbenv rehash
-
-ln -snfv ${HOME}/dotfiles/.pryrc ${HOME}/.pryrc
-echo "====== End ======"
-
-
 echo "====== Setup golang ======"
 git clone https://github.com/syndbg/goenv.git ~/.goenv
 # 最新版のgoをインストールする
