aa4e6af4437fdf74ed318edc811d730daa3ed946
kamillle <32205171+kamillle@users.noreply.github.com>
2026-01-12 21:10:20 +0900

    Update setup.sh

--- bin/setup.sh
+++ bin/setup.sh
@@ -11,8 +11,8 @@ esac
 
 if ! command -v brew &> /dev/null;then
   echo "======= Install Homebrew ======="
-
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ${HOME}/.zprofile
   eval "$(/opt/homebrew/bin/brew shellenv)"
 
